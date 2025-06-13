440c3912459ea2eedaef2dd12a78ccb2386c6f97
Sanjay Mehta <sanjay.mehta@hidubai.com>
2025-06-13 22:00:09 +0400

    file upload and download section completed

--- NodeJS The Complete Guide/controllers/shop.js
+++ NodeJS The Complete Guide/controllers/shop.js
@@ -1,5 +1,9 @@
 const Product = require('../models/product');
 const Order = require('../models/order');
+const fs = require('fs');
+const path = require('path');
+const rootDir = require('../util/path');
+const PDFDocument = require('pdfkit');
 
 exports.getProducts = (req, res, next) => {
   Product.find()
@@ -132,3 +136,66 @@ exports.postOrder = (req, res, next) => {
     return next(error);  
 });
 };
+
+exports.getInvoice = (req, res, next) => {
+    const orderId = req.params.orderId;
+
+    Order.findById(orderId).then(order => {
+        if(!order) {
+          return next(new Error('No order found.'));
+        }
+        if(order.user.userId.toString() !== req.user._id.toString()) {
+          return next(new Error('Unauthorized'));
+        }
+
+        const invoiceName = 'invoice-' + orderId + '.pdf';
+        const invoiceDir = path.join(__dirname, '..', 'data', 'invoices');
+        const invoicePath = path.join(invoiceDir, invoiceName);
+
+        if (!fs.existsSync(invoiceDir)) {
+          fs.mkdirSync(invoicePath, { recursive: true }); // recursively creates nested folders
+        }
+
+        const pdfDoc = new PDFDocument();
+        res.setHeader('Content-Type', 'application/pdf');
+        res.setHeader(
+          'Content-Disposition',
+           'inline; filename="'+ invoiceName +'"'
+          );
+        pdfDoc.pipe(fs.createWriteStream(invoicePath));
+        pdfDoc.pipe(res);
+
+        pdfDoc.fontSize(26).text('Invoice', {
+          underline: true
+        });
+        pdfDoc.text('---------------------');
+        let totalPrice = 0;
+        order.products.forEach(prod => {
+          totalPrice += prod.quantity * prod.product.price;
+          pdfDoc
+            .fontSize(14)
+            .text(
+              prod.product.title +
+                ' - ' +
+                prod.quantity +
+                ' x ' +
+                '$' +
+                prod.product.price
+            );
+        });
+        pdfDoc.text('---');
+        pdfDoc.fontSize(20).text('Total Price: $' + totalPrice);
+        pdfDoc.end();
+        // fs.readFile(invoicePath, (err, data) => {
+        //   if(err) {
+        //     return next(err);
+        //   }
+        //   res.setHeader('Content-Type', 'application/pdf');
+        //   res.setHeader('Content-Disposition', 'inline; filename="'+ invoiceName +'"');
+        //   res.send(data);
+        // });
+        // const file = fs.createReadStream(invoicePath);
+        // file.pipe(res);
+      })
+    .catch(err => next(err));
+}
