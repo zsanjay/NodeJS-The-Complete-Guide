7d186203c734f8be9fabd4a1b7ce1d12be69014f
Sanjay Mehta <sanjay.mehta@hidubai.com>
2025-06-14 11:29:33 +0400

    Pagination and Async Request Section Completed

--- NodeJS The Complete Guide/controllers/shop.js
+++ NodeJS The Complete Guide/controllers/shop.js
@@ -5,27 +5,62 @@ const path = require('path');
 const rootDir = require('../util/path');
 const PDFDocument = require('pdfkit');
 
+const ITEMS_PER_PAGE = 1;
+
 exports.getProducts = (req, res, next) => {
+  const page = +req.query.page || 1;
+  let totalItems;
+
   Product.find()
+  .countDocuments()
+  .then(numProducts => {
+    totalItems = numProducts;
+    return Product.find()
+    .skip((page - 1) * ITEMS_PER_PAGE)
+    .limit(ITEMS_PER_PAGE);
+  })
   .then(products => {
     res.render('shop/product-list', {
       prods: products,
       pageTitle: 'All Products',
-      path: '/products'
+      path: '/products',
+      currentPage: page,
+      hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+      hasPreviousPage: page > 1,
+      nextPage: page + 1,
+      previousPage: page - 1,
+      lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE) // if 11/2 which is 5.5, ceil will give 6.
     });
-  });
+  })
+  .catch(err => next(err));
 };
 
 exports.getIndex = (req, res, next) => {
+  const page = +req.query.page || 1;
+  let totalItems;
+
   Product.find()
+  .countDocuments()
+  .then(numProducts => {
+    totalItems = numProducts;
+    return Product.find()
+    .skip((page - 1) * ITEMS_PER_PAGE)
+    .limit(ITEMS_PER_PAGE);
+  })
   .then(products => {
     res.render('shop/index', {
       prods: products,
       pageTitle: 'Shop',
       path: '/',
-      csrfToken: req.csrfToken()
+      currentPage: page,
+      hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+      hasPreviousPage: page > 1,
+      nextPage: page + 1,
+      previousPage: page - 1,
+      lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE) // if 11/2 which is 5.5, ceil will give 6.
     });
-  });
+  })
+  .catch(err => next(err));
 };
 
 exports.getCart = (req, res, next) => {
